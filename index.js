f479a31d107727ca5fd5ea5af4daf1838c6da3be
Muhammad Mudasir Chandio <159702925+Mudasic1@users.noreply.github.com>
2024-04-17 20:12:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+// Declare a constant 'answer' and assign it to the result of inquirer.prompt function
+const answer = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: "
+    }]);
+const words = answer.Sentence.trim().split(" ");
+// print the array of the words to the console
+console.log(words);
+// Print the words count of the sentence to the console
+console.log(`your sentence word count is ${words.length}`);
